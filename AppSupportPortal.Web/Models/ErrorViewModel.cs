85f10408fdd2b90c0e29c200552145d63cd8fe66
Philip Miller <Philip.Miller@durham.gov.uk>
2026-05-26 10:15:42 +0100

    Initial ASP.NET Core MVC solution setup

---
+++ AppSupportPortal.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AppSupportPortal.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
