44eb842e79d35476896e5c6c86f3a32e12274042
Philip Miller <Philip.Miller@durham.gov.uk>
2026-06-19 17:35:59 +0100

    Fixed connection errors to Postgre db. Added CreatedAt column to users table and Users index page.

--- SeDevOps.Data/DesignTimeDbContextFactory.cs
+++ SeDevOps.Data/DesignTimeDbContextFactory.cs
@@ -10,7 +10,8 @@ namespace SeDevOps.Data
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
             optionsBuilder.UseNpgsql(
-                "Host=localhost;Port=5432;Database=SeDevOpsDb;Username=postgres;Password=DCC_LongLegs_2026!");
+                "Host=localhost;Port=5432;Database=se_devops_local;Username=postgres;Password=DCC_LongLegs_2026!;SSL Mode=Disable"
+);
 
             return new AppDbContext(optionsBuilder.Options);
         }
