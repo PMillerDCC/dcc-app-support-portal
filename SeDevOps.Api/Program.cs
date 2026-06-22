a3abc3c2c4f6edac4234d583261d6cf75d1163ab
Philip Miller <Philip.Miller@durham.gov.uk>
2026-06-22 18:13:56 +0100

    Modified Program.cs in API layer to update Render database.

--- SeDevOps.Api/Program.cs
+++ SeDevOps.Api/Program.cs
@@ -19,7 +19,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-// Trigger Render refresh
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
