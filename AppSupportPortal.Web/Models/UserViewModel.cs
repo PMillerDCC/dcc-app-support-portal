cd74d6601d94fa6b4b5b8f1a708b17a051441df7
Philip Miller <Philip.Miller@durham.gov.uk>
2026-06-23 09:00:16 +0100

    Added welcome message to homepage, updated UserViewModel for labels displayed on User page.

--- AppSupportPortal.Web/Models/UserViewModel.cs
+++ AppSupportPortal.Web/Models/UserViewModel.cs
@@ -6,10 +6,13 @@ namespace AppSupportPortal.Web.Models
     {
         public int Id { get; set; }
         [Required]
+        [Display(Name = "User Name")]
         public string UserName { get; set; } = string.Empty;
+        [Display(Name = "Display Name")]
         public string DisplayName { get; set; } = string.Empty;
         [Required]
         public string Email { get; set; } = string.Empty;
+        [Display(Name = "Role Type")]
         public string RoleName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
     }
